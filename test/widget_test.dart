5bd9af907194dcc742a5dd8a9aeae630ea6e0aca
Tanmay <tanmay2712d@gmail.com>
2025-02-06 16:47:06 +0530

    Test case

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -16,15 +16,15 @@ void main() {
     await tester.pumpAndSettle();
 
     // Verify that our counter starts at 0.
-    expectLater(find.text('0'), findsOneWidget);
-    expectLater(find.text('1'), findsNothing);
+    expect(find.text('0'), findsOneWidget);
+    expect(find.text('1'), findsNothing);
 
     // Tap the '+' icon and trigger a frame.
     await tester.tap(find.byIcon(Icons.add));
     await tester.pump();
 
     // Verify that our counter has incremented.
-    expectLater(find.text('0'), findsNothing);
-    expectLater(find.text('1'), findsOneWidget);
+    expect(find.text('0'), findsNothing);
+    expect(find.text('1'), findsOneWidget);
   });
 }
