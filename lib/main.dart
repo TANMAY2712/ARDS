6818f6d25d58180a7a4fb7c26acbbb72d3bfddf2
Tanmay <tanmay2712d@gmail.com>
2025-02-06 17:09:09 +0530

    Test case

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,39 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginPage(),
+      home: CounterScreen(),
+    );
+  }
+}
+
+class CounterScreen extends StatefulWidget {
+  @override
+  _CounterScreenState createState() => _CounterScreenState();
+}
+
+class _CounterScreenState extends State<CounterScreen> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Counter Test')),
+      body: Center(
+        child: Text(
+          '$_counter', // Displays counter value
+          style: TextStyle(fontSize: 40),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        child: Icon(Icons.add), // Button to increment
+      ),
     );
   }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,24 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:ards/main.dart';
+import 'package:ards/main.dart'; // Update with the correct import path for your app
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
+    // Load the app
     await tester.pumpWidget(MyApp());
 
-    // Verify that our counter starts at 0.
+    // Verify counter starts at "0"
     expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
+    // Find the floating action button
+    final Finder fab = find.byIcon(Icons.add);
+    expect(fab, findsOneWidget);
+
+    // Tap the button and trigger a frame
+    await tester.tap(fab);
     await tester.pump();
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
+    // Verify counter increments to "1"
     expect(find.text('1'), findsOneWidget);
   });
 }
